82cb959a56a8c94e41148948afb847cd3359e8a8
egorcha174 <83664062+egorcha174@users.noreply.github.com>
2025-11-01 09:48:02 +0500

    chore: Update dependencies to use ESM CDN
    Switches from a custom CDN to esm.sh for resolving frontend dependencies. This simplifies the import map and ensures consistent versioning.

--- components/DeviceIcon.tsx
+++ components/DeviceIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Icon } from "https://esm.sh/@iconify/react@5.0.1";
+import { Icon } from "@iconify/react";
 import { DeviceType, CardSize } from '../types';
 
 interface DeviceIconProps {
